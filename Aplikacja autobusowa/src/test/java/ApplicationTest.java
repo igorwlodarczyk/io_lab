68f1f8fa4e4d7ffa185a3ee649a6fbaf4d072f1e
MKazmierczak02 <mmichal.kkazmierczak@gmail.com>
2024-01-09 18:12:52 +0100

    Tests

---
+++ Aplikacja autobusowa/src/test/java/ApplicationTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void testAdd() {
+        int result = 3+5;
+        Assertions.assertEquals(8, result);
+    }
+
+
+}
